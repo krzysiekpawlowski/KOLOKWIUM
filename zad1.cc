9dd3413fba6db2f9c9057560153de5defe25158b
krzysiekpawlowski <51051448+krzysiekpawlowski@users.noreply.github.com>
2019-05-26 17:25:54 +0200

    Add files via upload

---
+++ zad1.cc
@@ -0,0 +1,27 @@
+#include <omp.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// Najpierw skompiluj i uruchom ponizszy kod. 
+// Tak zmodyfikuj blok rownolegly, zeby wczytana
+// z pliku test.txt zawartosc byla poprawna.
+
+int main () {
+
+    fstream plik("test.txt");
+    string tmp, buf;
+
+#pragma omp parallel num_threads(4)
+{
+    while ( getline( plik, tmp ) ) {
+	#pragma omp critical
+        buf += tmp + '\n';
+    }
+}
+
+cout << buf << endl;
+
+}
+
